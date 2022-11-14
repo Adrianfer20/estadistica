40cb3fda4ccb78a443e00b34ea82da9742f6abb8
Adrianfer <35809235+Adrianfer20@users.noreply.github.com>
2022-11-14 11:31:28 -0400

    commit 1.0.0.1
    Mejora de Grid sistem dentro de los RowWrapper

--- js/main.js
+++ js/main.js
@@ -61,14 +61,39 @@ const showResult = (result) => {
         $divRow.className = 'w-full flex flex-wrap text-sm border border-indigo-600 rounded shadow px-2 py-1 mb-1';
         $divRow.innerHTML = `
         <div class="w-4/12 md:w-2/12 grid grid-cols-3 my-1">
-                            <h2 class="col-span-1 text-center self-center">${numberXi}</h2>
-                            <h2 class="col-span-1 text-center self-center">${getShowFi(result.Fi, numberXi)}</h2>
-                            <h2 class="col-span-1 text-center self-center">${getShowFa(result.Fa, index)}</h2>
+                            <div class="col-span-1 flex items-center justify-center">
+                                <h2 class=" self-center">${numberXi}</h2>
+                            </div>
+                            <div class="col-span-1 flex items-center justify-center">
+                            <h2 class=" self-center">${getShowFi(result.Fi, numberXi)}</h2></div>
+                            
+                            <div class="col-span-1 flex items-center justify-center">
+                            <h2 class=" self-center">${getShowFa(result.Fa, index)}</h2>
+                            </div>
                         </div>
-                        <h2 class="w-4/12 md:w-2/12 my-1">${getShowFr(result,index, numberXi)}</h2>
-                        <h2 class="w-4/12 md:w-3/12 text-center self-center my-1">${getShowFra(result.Fra, index)}</h2>
-                        <h2 class="w-6/12 md:w-2/12 text-center self-center my-1">${getShowFp(result.Fr, result.Fp, index)}</h2>
-                        <h2 class="w-6/12 md:w-3/12 text-center self-center my-1">${getShowFap(result.Fap, index)}</h2>
+
+                        <div class="w-4/12 md:w-2/12 my-1">
+                            <div class="flex items-center justify-center"></div>
+                            <h2>${getShowFr(result,index, numberXi)}</h2>
+                        </div>
+
+                        <div class="w-4/12 md:w-3/12 text-center my-1">
+                            <div class="flex items-center justify-center">
+                                <h2>${getShowFra(result.Fra, index)}</h2>
+                            </div>
+                        </div>
+
+                        <div class="w-6/12 md:w-2/12 text-center my-1>
+                            <div class="flex items-center justify-center">
+                                <h2 ">${getShowFp(result.Fr, result.Fp, index)}</h2>
+                            </div>
+                        </div>
+
+                        <div class="w-6/12 md:w-3/12 text-center my-1">
+                            <div class="flex items-center justify-center">
+                                <h2 >${getShowFap(result.Fap, index)}</h2>
+                            </div>
+                        </div>
         `;
         $fragment.appendChild($divRow);
     });
@@ -78,8 +103,8 @@ const showResult = (result) => {
 const showOrderNumber = () => {
     const {value} = document.getElementById('input-orderNumber');
     if(!value) return alert('Debe introducir obligatoriamente al menos un valor');
-     const stringNumber = "7.24,7.25,7.26,7.27,7.27,7.28,7.28,7.29,7.29,7.29,7.30,7.30,7.30, 7.31, 7.31,7.32,7.32, 7.32, 7.32,7.32,7.33,7.33,7.33,7.34, 7.34, 7.34,7.34,7.35,7.35,7.35,7.35,7.35,7.35,7.35,7.35,7.36,7.36,7.36,  7.36,7.36,7.36,7.37,7.37,7.37,7.38,7.38,7.38,7.39,7.39,7.39,7.40,7.40,7.40,7.41,7.41";
-    const result = getProcess(stringNumber);
+    //  const stringNumber = "7.24,7.25,7.26,7.27,7.27,7.28,7.28,7.29,7.29,7.29,7.30,7.30,7.30, 7.31, 7.31,7.32,7.32, 7.32, 7.32,7.32,7.33,7.33,7.33,7.34, 7.34, 7.34,7.34,7.35,7.35,7.35,7.35,7.35,7.35,7.35,7.35,7.36,7.36,7.36,  7.36,7.36,7.36,7.37,7.37,7.37,7.38,7.38,7.38,7.39,7.39,7.39,7.40,7.40,7.40,7.41,7.41";
+    const result = getProcess(value);
     showXi(result.Xi);
     showResult(result);
 }
